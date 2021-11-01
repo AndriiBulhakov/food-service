9fb26dd0d59a5f084ee2e997964879c709e512ea
Andrii Bulhakov <andrijbulgakov5@gmail.com>
2021-11-01 20:51:39 +0200

    food modal

--- js/script.js
+++ js/script.js
@@ -37,4 +37,40 @@ window.addEventListener('DOMContentLoaded', () => {
             })
         }
     })
+
+    // Modal
+
+    const modalTrigger = document.querySelectorAll('[data-modal]');
+    const modalCloseBtn = document.querySelectorAll('[data-close]');
+    const modal = document.querySelector('.modal');
+
+    modalTrigger.forEach(button => {
+        button.addEventListener('click', () => {
+            modal.classList.add('show');
+            modal.classList.remove('hide');
+            document.body.style.overflow = 'hidden';
+        });
+    });
+
+    function closeModal () {
+        modal.classList.remove('show');
+        modal.classList.add('hide');
+        document.body.style.overflow = '';
+    };
+
+    modalCloseBtn.forEach(button => {
+        button.addEventListener('click', closeModal)
+    });
+
+    modal.addEventListener('click', (e) => {
+        if(e.target === modal){
+            closeModal();
+        }
+    })
+
+    document.addEventListener('keydown', (e) => {
+        if(e.code === 'Escape' && modal.classList.contains('show')){
+            closeModal();
+        };
+    });
 }) 
